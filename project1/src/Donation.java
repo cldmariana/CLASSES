8a434713120a288cc8e7de9952a615cbadbfa121
cldmariana <claudinomariana502@gmail.com>
2024-10-17 18:33:25 -0300

    donation algorithm

---
+++ project1/src/Donation.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Donation {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("[1] to donate $10");
+        System.out.println("[2] to donate $25");
+        System.out.println("[3] to donate $50");
+        System.out.println("[4] to donate a custom value");
+        System.out.println("[5] to cancel");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("You donated $10");
+                break;
+            case 2:
+                System.out.println("You donated $25");
+                break;
+            case 3:
+                System.out.println("You donated $50");
+                break;
+            case 4:
+                System.out.println("Enter the donation value: ");
+                int donation = scanner.nextInt();
+                System.out.println("You donated $" + donation);
+                break;
+            case 5:
+                System.out.println("You have cancelled the donation.");
+                break;
+        }
+    }
+}
