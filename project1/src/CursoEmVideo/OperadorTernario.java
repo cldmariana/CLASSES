a385d7f77de0dcdbc7e940852bbc4f822e0885c7
cldmariana <claudinomariana502@gmail.com>
2024-10-21 14:57:07 -0300

    poo

--- project1/src/CursoEmVideo/OperadorTernario.java
+++ project1/src/CursoEmVideo/OperadorTernario.java
@@ -5,8 +5,8 @@ public class OperadorTernario {
     public static void main(String[] args) {
 
         int n1, n2, r;
-        n1 = 5;
-        n2 = 4;
+        n1 = 9;
+        n2 = 7;
 
         /*
         ponto de interrogação pra verificar a expressão entre parênteses;
@@ -14,7 +14,9 @@ public class OperadorTernario {
         se falsa, acontece o lado direito
          */
 
-        r = (n1 > n2) ? 0 : 1;
+        //r = (n1 > n2) ? 0 : 1;
+        r = (n1 > n2) ? n1 * n2 : n2 + n1;
+        //r = (n1 > n2) ? n1 + n2 : n2 - n1;
 
         /*
         se n1 é maior que n2, r recebe 0
---
+++ project1/src/CursoEmVideoPOO/Caneta.java
@@ -0,0 +1,31 @@
+package CursoEmVideoPOO;
+
+public class Caneta {
+
+    String modelo, cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+
+
+    public void escrever() {
+
+    }
+
+    public void rabiscar() {
+
+    }
+
+    public void pintar() {
+
+    }
+
+    public void tampar() {
+
+    }
+
+    public void destampar() {
+
+    }
+}
